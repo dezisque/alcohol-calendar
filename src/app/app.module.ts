88d31927a7eb6527d601099c9867ff3974d6a5fc
Ivan Aderkin <ivan.aderkin@galaktika.ru>
2020-09-27 16:38:06 +0300

    add styles, logo and edit App-component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { InfoPageComponent } from './info-page/info-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CalendarComponent } from './calendar/calendar.component';
 
+
+registerLocaleData(localeRu, 'ru');
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoPageComponent,
+    CalendarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ru' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
